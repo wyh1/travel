56439253dd804843f7407a3dbd334b64125a0d57
11030 <1103043038@qq.com>
2019-11-07 23:10:34 +0800

    citys-ajax数据获取

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -1,93 +1,45 @@
 <template>
-  <div class="list">
-    <div class="area">
-      <div class="title border-topbottom">当前城市</div>
-      <div class="button-list">
-        <div class="button-wrapper">
-          <div class="button">北京</div>
+  <div class="list" ref="wrapper">
+    <div>
+      <div class="area">
+        <div class="title border-topbottom">当前城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="area">
-      <div class="title border-topbottom">热门城市</div>
-      <div class="button-list">
-        <div class="button-wrapper">
-          <div class="button">北京</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">北京</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">北京</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">北京</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">北京</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">北京</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">北京</div>
+      <div class="area">
+        <div class="title border-topbottom">热门城市</div>
+        <div class="button-list">
+          <div class="button-wrapper" v-for="item of hot" :key="item.id">
+            <div class="button">{{item.name}}</div>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="area">
-      <div class="title border-topbottom">A</div>
-      <div class="item-list">
-        <div class="item border-bottom">阿富汗</div>
-        <div class="item border-bottom">阿富汗</div>
-        <div class="item border-bottom">阿富汗</div>
-        <div class="item border-bottom">阿富汗</div>
-        <div class="item border-bottom">阿富汗</div>
-        <div class="item border-bottom">阿富汗</div>
-        <div class="item border-bottom">阿富汗</div>
-      </div>
-    </div>
-    <div class="area">
-      <div class="title border-topbottom">A</div>
-      <div class="item-list">
-        <div class="item border-bottom">阿富汗</div>
-        <div class="item border-bottom">阿富汗</div>
-        <div class="item border-bottom">阿富汗</div>
-        <div class="item border-bottom">阿富汗</div>
-        <div class="item border-bottom">阿富汗</div>
-        <div class="item border-bottom">阿富汗</div>
-        <div class="item border-bottom">阿富汗</div>
-      </div>
-    </div>
-    <div class="area">
-      <div class="title border-topbottom">A</div>
-      <div class="item-list">
-        <div class="item border-bottom">阿富汗</div>
-        <div class="item border-bottom">阿富汗</div>
-        <div class="item border-bottom">阿富汗</div>
-        <div class="item border-bottom">阿富汗</div>
-        <div class="item border-bottom">阿富汗</div>
-        <div class="item border-bottom">阿富汗</div>
-        <div class="item border-bottom">阿富汗</div>
-      </div>
-    </div>
-    <div class="area">
-      <div class="title border-topbottom">A</div>
-      <div class="item-list">
-        <div class="item border-bottom">阿富汗</div>
-        <div class="item border-bottom">阿富汗</div>
-        <div class="item border-bottom">阿富汗</div>
-        <div class="item border-bottom">阿富汗</div>
-        <div class="item border-bottom">阿富汗</div>
-        <div class="item border-bottom">阿富汗</div>
-        <div class="item border-bottom">阿富汗</div>
+      <div class="area" v-for="(item, key) of cities" :key="key">
+        <div class="title border-topbottom">{{key}}</div>
+        <div class="item-list">
+          <div class="item border-bottom" v-for="innerItem of item" :key="innerItem.id">
+            {{innerItem.name}}
+            </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import BScroll from 'better-scroll'
 export default {
-  name: 'CityList'
+  name: 'CityList',
+  mounted () {
+    this.scroll = new BScroll(this.$refs.wrapper)
+  },
+  props: {
+    hot: Array,
+    cities: Object
+  }
 }
 </script>
 
@@ -103,7 +55,7 @@ export default {
   .list
     position absolute
     overflow hidden
-    top 1.58rem
+    top 1.60rem
     left 0
     right 0
     bottom 0
