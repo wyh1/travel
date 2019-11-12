99008088cd1bc0f363097494fd6d2f448ce6057d
11030 <1103043038@qq.com>
2019-11-12 11:43:12 +0800

    detail-header

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,18 +2,22 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
-    <div class="content"></div>
+    <div class="content">
+      <detail-list></detail-list>
+    </div>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
   }
 }
 </script>
